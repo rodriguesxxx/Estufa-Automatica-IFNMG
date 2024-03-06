d72301a5339ee67f39d6b82382b953a8a7875dd0
rodriguesxxx <rodriguesx.daniel89@gmail.com>
2024-03-06 16:35:13 -0300

    backup

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,11 @@
 #include <Arduino.h>
+#include "Rele.h"
 
-// put function declarations here:
-int myFunction(int, int);
+Rele rele(7);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  rele.powerOn();
 }
